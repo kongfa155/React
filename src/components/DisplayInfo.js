039681429ed64b1c08362c9741050b669fa820f4
Dao Cong Pha <mongpha90@gmail.com>
2025-09-10 23:02:22 +0700

    Picture store in React

--- src/components/DisplayInfo.js
+++ src/components/DisplayInfo.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "./DisplayInfo.scss";
+import logo from "../logo.svg";
 class DisplayInfo extends React.Component{
     state = {
         isShow: true,
@@ -16,6 +17,7 @@ class DisplayInfo extends React.Component{
         return(
             
             <div className = 'display-infor-container'>
+                <img src={logo}/>
                 <div>
                     <span onClick={(event)=> {
                         this.handleShowHide(event)
