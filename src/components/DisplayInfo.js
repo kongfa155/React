5aad28a15ec394f1ad86250292bf63ebfeb97256
Dao Cong Pha <mongpha90@gmail.com>
2025-09-10 22:09:12 +0700

    Passing Array/Object as props

--- src/components/DisplayInfo.js
+++ src/components/DisplayInfo.js
@@ -2,12 +2,19 @@ import React from "react";
 
 class DisplayInfo extends React.Component{
     render(){
-        const {name, age} = this.props 
+        const {listUsers} = this.props 
         return(
 
             <div>
-                <div>My name's {name}</div>
-                <div>My age's  {age}</div>
+                {listUsers.map((user)=>{
+                    return (
+                        <div key={user.id}>
+                            <div>My name's {user.name}</div>
+                            <div>My age's  {user.age}</div>
+                            <hr/>
+                        </div>
+                    )
+                })}
             </div>
         )
     }
--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -4,16 +4,20 @@ import React from "react";
 import UserInfo from "./UserInfo";
 import DisplayInfo from "./DisplayInfo";
 class MyComponent extends React.Component {
-    
+    state = {
+        listUsers: [
+            {id: 1, name:"Cong Pha", age:20},
+            {id: 2, name:"Tra Nhu", age:19},
+            {id: 3, name:"TMDEY", age:1}
+        ]
+    }
     //JSX
     render(){
         return (
             <div> 
                 <UserInfo></UserInfo>
                 <br/>
-                <DisplayInfo name="Cong Pha" age="20"/>
-                <hr/>
-                <DisplayInfo name="Tra Nhu" age="19"/>
+                <DisplayInfo listUsers={this.state.listUsers}/>
             </div>
             
         );
