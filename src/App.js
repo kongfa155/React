6a4c57df0436b92b49005cc7f5812095a64fa5b5
Dao Cong Pha <mongpha90@gmail.com>
2025-09-11 18:31:00 +0700

    New Branch Web

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 
 import './App.scss';
-
+import Header from './components/Header/Header';
 
 
 const App = () => {
  
 
   return (
-    <div className='app-container'> Hello
+    <div className='app-container'>
+        <Header/>
     </div>
     
   )
---
+++ src/components/Header/Header.js
@@ -0,0 +1,35 @@
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+
+const Header = ()=> {
+  return (
+    <>
+      <Navbar bg="light" data-bs-theme="light">
+        <Container>  
+        <Navbar.Brand href="#home">Hoi Dan It</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#link">User</Nav.Link>
+            <Nav.Link href="#link">Link</Nav.Link>
+          </Nav>
+          <Nav>
+            <NavDropdown title="Setting" id="basic-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">Log in</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">
+                Log out
+              </NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.1">User Info</NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+        </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </>
+  );
+}
+
+export default Header;
