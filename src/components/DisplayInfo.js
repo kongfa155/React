c63c4050d2b4b31605fc46143dd120ad16234a91
Dao Cong Pha <mongpha90@gmail.com>
2025-09-11 09:03:52 +0700

    Life Cycle

--- src/components/DisplayInfo.js
+++ src/components/DisplayInfo.js
@@ -10,8 +10,20 @@ class DisplayInfo extends React.Component{
             isShow: !this.state.isShow,
         })
     }
+    componentDidUpdate(prevProps, preStates, snapshot){
+        console.log("call me componentDidUpdate", this.props, prevProps);
+        if(this.props.listUsers.length === 5){
+            alert("You got 5 User");
+        }
+    }
+    componentDidMount() {
+        console.log("call me component did mount");
+        setTimeout(()=> {
+            document.title = "Hello"
+        }, 5000)
+    }
     render(){
-        
+        console.log("call me render")
         const {listUsers} = this.props 
         // console.log(listUsers);
         return(
