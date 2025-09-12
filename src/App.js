263305988825f713911bdf4a93e6d2d7ca8d7298
Dao Cong Pha <mongpha90@gmail.com>
2025-09-12 13:46:19 +0700

    Link router

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 
 import './App.scss';
 import Header from './components/Header/Header';
-
+import { Link } from 'react-router-dom';
 
 const App = () => {
  
@@ -9,6 +9,18 @@ const App = () => {
   return (
     <div className='app-container'>
         <Header/>
+        <div>
+            Test Link
+            <div>
+                <button>
+                    <Link to="/users">Go to User Page</Link>
+                     
+                     </button>
+                <button> 
+                    <Link to="/admins">Go to Admin Page</Link>
+                </button>
+            </div>
+        </div>
     </div>
     
   )
